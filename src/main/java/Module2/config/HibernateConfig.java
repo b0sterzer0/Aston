b9bf320692ad2282f2e925f765051811080c2451
b0sterzer0 <zerbster010@gmail.com>
2026-03-03 16:56:22 +0300

    Добавлены настройки для Hibernate и модель User

---
+++ src/main/java/Module2/config/HibernateConfig.java
@@ -0,0 +1,46 @@
+package Module2.config;
+
+import Module2.models.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class HibernateConfig {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        String dbUrl = constructUrlToDatabase();
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        configuration.setProperty("hibernate.connection.url", dbUrl);
+        configuration.addAnnotatedClass(User.class);
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    private static String constructUrlToDatabase() {
+        try (InputStream in = HibernateConfig.class.getClassLoader().getResourceAsStream("database.properties")) {
+            if (in == null) throw new IllegalStateException("Cannot find database properties file.");
+            Properties dbProperties = new Properties();
+            dbProperties.load(in);
+            String host = dbProperties.getProperty("db.host");
+            String port = dbProperties.getProperty("db.port");
+            String dbName = dbProperties.getProperty("db.db_name");
+            String username  = dbProperties.getProperty("db.username");
+            String password = dbProperties.getProperty("db.password");
+
+            // Имя пользователя и пароль в url!!!
+            String urlFormat = "jdbc:postgresql://%s:%s/%s?user=%s&password=%s";
+            return String.format(urlFormat, host, port, dbName, username, password);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Loading database properties failed.", e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
---
+++ src/main/java/Module2/models/User.java
@@ -0,0 +1,59 @@
+package Module2.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "age")
+    private int age;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    public User() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
