d0c84fd559a242582d1ce9d4366c1ef58b405c81
b0sterzer0 <zerbster010@gmail.com>
2026-03-04 16:57:41 +0300

    Добавлены зависимости slf4j и logback в pom.xml. Добавлено лоигрование для UserService и UserDAO

--- src/main/java/Module2/dao/UserDAO.java
+++ src/main/java/Module2/dao/UserDAO.java
@@ -4,20 +4,27 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import Module2.config.HibernateConfig;
 import Module2.models.User;
 
 public class UserDAO implements DaoInterface<User> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserDAO.class);
+
     public User create(User user) {
         Transaction tx = null;
         try (Session session = HibernateConfig.getSessionFactory().openSession()) {
+            LOGGER.info("Creating User. Username: {} Email: {}", user.getName(), user.getEmail());
             tx = session.beginTransaction();
             session.persist(user);
             tx.commit();
+            LOGGER.info("User created with id: {}",  user.getId());
             return user;
         } catch (Exception e) {
             if  (tx != null) tx.rollback();
+            LOGGER.error("User creation failed", e);
             throw e;
         }
     }
@@ -37,12 +44,20 @@ public class UserDAO implements DaoInterface<User> {
     public User update(User userForUpdate) {
         Transaction tx = null;
         try (Session session = HibernateConfig.getSessionFactory().openSession()) {
+            LOGGER.info("Updating User. ID: {} Username: {} Email: {} Age: {}",
+                    userForUpdate.getId(),
+                    userForUpdate.getName(),
+                    userForUpdate.getEmail(),
+                    userForUpdate.getAge()
+            );
             tx = session.beginTransaction();
             User updatedUser = session.merge(userForUpdate);
             tx.commit();
+            LOGGER.info("Successful updating User with ID: {}",  userForUpdate.getId());
             return updatedUser;
         } catch (Exception e) {
             if (tx != null) tx.rollback();
+            LOGGER.error("User update failed with ID: {}",  userForUpdate.getId(), e);
             throw e;
         }
     }
@@ -50,14 +65,20 @@ public class UserDAO implements DaoInterface<User> {
     public User delete(long id) {
         Transaction tx = null;
         try (Session session = HibernateConfig.getSessionFactory().openSession()) {
+            LOGGER.info("Deleting User with ID: {}", id);
             tx = session.beginTransaction();
             User user = session.find(User.class, id);
-            if (user == null) return null;
+            if (user == null) {
+                tx.rollback();
+                return null;
+            }
             session.remove(user);
             tx.commit();
+            LOGGER.info("Successful deleting User with ID: {}",  user.getId());
             return user;
         } catch (Exception e) {
             if (tx != null) tx.rollback();
+            LOGGER.error("Deleting User with ID: {} failed",  id, e);
             throw e;
         }
     }
--- src/main/java/Module2/services/UserService.java
+++ src/main/java/Module2/services/UserService.java
@@ -5,10 +5,13 @@ import Module2.dto.UserDTO;
 import Module2.models.User;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class UserService {
+    private final static Logger LOGGER = LoggerFactory.getLogger(UserService.class);
     private final UserDAO userDAO;
 
     public UserService(UserDAO userDAO) {
@@ -35,27 +38,38 @@ public class UserService {
     }
 
     public List<UserDTO> getUsers() {
+        LOGGER.debug("Request to get all users");
         List<User> usersFromDB = userDAO.getAll();
+        if (usersFromDB.isEmpty()) LOGGER.debug("Returned User list is empty");
         return usersFromDB.stream()
                 .map(this::getUserDtoFromUser)
                 .toList();
     }
 
     public UserDTO getUser(long id) {
+        LOGGER.debug("Request to get User with id {}", id);
         User user = userDAO.get(id);
-        if (user == null) throw new IllegalArgumentException("User with id " + id + " is not found");
+        if (user == null) {
+            LOGGER.warn("User with id {} not found", id);
+            throw new IllegalArgumentException("User with id " + id + " is not found");
+        }
         return getUserDtoFromUser(user);
     }
 
     public UserDTO createUser(UserDTO userDTO) {
+        LOGGER.info("Request to create new User");
         User user = getUserFromDTO(userDTO);
         User addedUser = userDAO.create(user);
         return getUserDtoFromUser(addedUser);
     }
 
     public UserDTO updateUser(long id, UserDTO userDTO) {
+        LOGGER.info("Request to update User with id {}", id);
         User userForUpdate = userDAO.get(id);
-        if  (userForUpdate == null) throw new IllegalArgumentException("User with id " + id + " is not found");
+        if  (userForUpdate == null) {
+            LOGGER.warn("Updating failed. User with id {} not found", id);
+            throw new IllegalArgumentException("User with id " + id + " is not found");
+        }
         userForUpdate.setName(userDTO.getName());
         userForUpdate.setEmail(userDTO.getEmail());
         userForUpdate.setAge(userDTO.getAge());
@@ -64,6 +78,7 @@ public class UserService {
     }
 
     public UserDTO deleteUser(long id) {
+        LOGGER.info("Request to delete User with id {}", id);
         User user = userDAO.delete(id);
         if (user == null) throw new IllegalArgumentException("Can not delete User with id " + id);
         return getUserDtoFromUser(user);
